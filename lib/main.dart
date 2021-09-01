02750263ca7a3207e96f9e6c9f89349b32983bc4
theselfobsessedgeek <shubhamahlawatg@gmail.com>
2021-09-01 14:21:12 +0530

    Splash Screen added

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:book_perception/Screens/landingPage.dart';
 import 'package:book_perception/auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,15 +11,39 @@ void main() async{
 
 }
 
+class Splash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 6,
+      image: Image.asset('assets/images/boomks.jpeg'),
+      title: Text("Book Perception",
+      style: TextStyle(
+        fontWeight: FontWeight.w300,
+            fontSize: 50,
+      ),
+
+      ),
+      photoSize: 150.0,
+      navigateAfterSeconds: LandingPage(
+        auth: Auth(),
+      ),
+    );
+  }
+}
+
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-      home: LandingPage(
-        auth: Auth(),
-      ),
+      checkerboardOffscreenLayers: false,
+
+      home: Splash()
+
     );
   }
 }
+
